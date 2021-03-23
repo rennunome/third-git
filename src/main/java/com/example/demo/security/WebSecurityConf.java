df3c73d15946b93dc207844dad82596a8eb3e767
rennunome <r_nunome@emyus.jp>
2021-03-23 18:30:11 +0900

    Modified WebSecurityConf.java, login.html

--- src/main/java/com/example/demo/security/WebSecurityConf.java
+++ src/main/java/com/example/demo/security/WebSecurityConf.java
@@ -1,10 +1,7 @@
 package com.example.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSourceAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,22 +30,15 @@ public class WebSecurityConf extends WebSecurityConfigurerAdapter{
 			.and()
 			//ブラウザ上でログイン情報とパスワードを求めるもの
 			.formLogin().loginPage("/login").defaultSuccessUrl("/top", true).failureUrl("/error").usernameParameter("id").passwordParameter("password").permitAll();
-
-		AuthenticationManager a = this.authenticationManager();
-        if (a instanceof ProviderManager) {
-            ProviderManager a2 = (ProviderManager)a;
-            a2.getProviders().forEach(p -> {
-                if (p instanceof MessageSourceAware) {
-                    ((MessageSourceAware)p).setMessageSource(s);
-                }
-            });
-        }
 	}
+
+
 	//パスワードをハッシュ化するためのメソッド
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 
-	}}
+	}
+	}
 
 
